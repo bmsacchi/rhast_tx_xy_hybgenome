5e7b3a5230bcc7cff18427537f8a05be5594865e
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2023-11-20 11:42:27 -0500

    gitignore fixin

--- gff/gff_parse_tx.sh
+++ gff/gff_parse_tx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 awk '{if (($1 == "X") && ($4 < 220000000)) print}' maternal_liftoff.gff  > justX.gff
 sed -E 's/NChap2_final_/X_NChap2_final_/g' justX.gff > justX_rename.gff
+cat paternal_liftoff.gff justX_rename.gff > merged_TX_combined.gff
+gffread merged_TX_combined.gff -T -o merged_TX_combined.gtf
